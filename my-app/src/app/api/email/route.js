9582376dde68b3aab05a6154a2b95fbfa5f4ddff
DavidThorn03 <b00152842@mytudublin.ie>
2024-11-28 10:33:32 +0000

    email fix

--- my-app/src/app/api/email/route.js
+++ my-app/src/app/api/email/route.js
@@ -1,31 +1,34 @@
-var nodemailer = require('nodemailer');
+export async function GET(req, res) {
+  var nodemailer = require('nodemailer');
 
-const { searchParams } = new URL(req.url);
-const user = searchParams.get('user');
+  const { searchParams } = new URL(req.url);
+  const user = searchParams.get('user');
+  
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'davythornton@gmail.com',
+      pass: 'enter app password here'
+    }
+  });
+  
+  var mailOptions = {
+    from: 'davythornton@gmail.com',
+    to: user,
+    subject: 'KremeKrispy Order',
+    text: 'Thank you for placing an order with Krispy Kreme. Your order will be ready for collection in 15 minutes.'
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'davythornton@gmail.com',
-    pass: 'enter app password here'
-  }
-});
 
-var mailOptions = {
-  from: 'davythornton@gmail.com',
-  to: user,
-  subject: 'KremeKrispy Order',
-  text: 'Thank you for placing an order with Krispy Kreme. Your order will be ready for collection in 15 minutes.'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-
+}
 
 
 
